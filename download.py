5f0d2543314da2d99443a7d694232f8a815fd281
da-ket <daket.duet@gmail.com>
2024-06-16 14:43:45 +0900

    import advertising dataset

--- download.py
+++ download.py
@@ -2,4 +2,5 @@ import os
 
 os.system('kaggle datasets download -d aasheesh200/framingham-heart-study-dataset --path dataset --unzip')
 os.system('kaggle datasets download -d johnsmith88/heart-disease-dataset --path dataset --unzip')
+os.system('kaggle datasets download -d ashydv/advertising-dataset --path dataset --unzip')
 
